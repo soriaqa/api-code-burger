cda3dfdb355147eb951ebc7b92e211a92ef13e11
Rodolfo Mori <rod_dolpho@hotmail.com>
2022-02-28 10:18:05 -0300

    finished project / without mongo / npm start added

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,6 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'postgres',
-  database: 'codeburger',
+  url: 'postgres://yybuidvrzxwfvo:160b0193681956db62151a120eb05b35ebf492f277532ab90fe2fc0b4ae88d4b@ec2-3-219-204-29.compute-1.amazonaws.com:5432/d1jeej75cjceir'
   define: {
     timespamps: true,
     underscored: true,
